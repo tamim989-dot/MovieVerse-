3a962092eb21f4e44168e1dbf705393b151c4837
Md Tanim <tamimmd003@gmail.com>
2026-08-08 15:15:35 +0600

    configure cloudinary

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ import cloudinary
 cloudinary.config(
     cloud_name="mlnj3amk",
     api_key="149813588799295",
-    api_secret="তোমার নতুন secret"
+    api_secret="1CBOpoVnJP_u4iTXVek_xDfoRg8 "
 )
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
