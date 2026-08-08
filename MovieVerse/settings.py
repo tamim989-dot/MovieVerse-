15e48313c5c608e249d5bab5cd741525192d4953
Md Tanim <tamimmd003@gmail.com>
2026-08-08 11:52:33 +0600

    fix media files serving

--- MovieVerse/settings.py
+++ MovieVerse/settings.py
@@ -88,3 +88,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- MovieVerse/urls.py
+++ MovieVerse/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -13,4 +15,5 @@ urlpatterns = [
 ]
 
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
